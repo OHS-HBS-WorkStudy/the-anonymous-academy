ab7146d08d412af1836fec47a8259a43823f920c
ChinchillaCoder <70586375+Idk3333@users.noreply.github.com>
2025-05-07 18:28:42 +0000

    message

--- client/src/Manager.js
+++ client/src/Manager.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Route, Routes} from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 import Home from './screens/Home.js';
 import Account from './screens/Account.js';
@@ -11,32 +11,32 @@ import NewThread from './screens/NewThread.js';
 import Navigator from './modules/Navigator.js';
 
 export default function Manager() {
-
   const foundUser = JSON.parse(sessionStorage.getItem("foundUser"));
 
-    if (foundUser && foundUser.pref && foundUser.pref.darkMode === true) {
-        document.body.classList.add("dark"); 
-    } else {
-        document.body.classList.remove("dark"); 
-    }
+  useEffect(() => {
+    document.title = "Anonymous Academy";
+  }, []);
+
+  if (foundUser && foundUser.pref && foundUser.pref.darkMode === true) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
 
   return (
     <>
-    <Navigator />
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/" element={<ThreadList />} />
-
-      <Route path="/account/overview" element={<Account />} />
-      <Route path="/account/activity" element={<Account />} />
-      <Route path="/account/settings" element={<Account />} />
-
-
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/newthread" element={<NewThread />} />
-      <Route path="/thread/:threadId" element={<Thread />} />
-    </Routes>
+      <Navigator />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<ThreadList />} />
+        <Route path="/account/overview" element={<Account />} />
+        <Route path="/account/activity" element={<Account />} />
+        <Route path="/account/settings" element={<Account />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/newthread" element={<NewThread />} />
+        <Route path="/thread/:threadId" element={<Thread />} />
+      </Routes>
     </>
   );
 }
